73635dbf8250eb7d4661f07414136f49914152d3
Teddy Sears <tjsearsz@users.noreply.github.com>
2023-08-31 01:39:45 +0200

    adding mutation for changing businessowner value
    - Adding mutation that enables or disables user for being business owner.

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -3,6 +3,8 @@ import { UserService } from './user.service.js';
 import { User } from './entities/user.entity.js';
 import { CreateUserInput } from './dto/create-user.input.js';
 import { UpdateUserInput } from './dto/update-user.input.js';
+import { CurrentUser } from '../decorators/currentUser.js';
+import { IUserSummary } from '../auth/interfaces/IUserSummary.js';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -34,5 +36,16 @@ export class UserResolver {
   @Mutation(() => User)
   removeUser(@Args('id', { type: () => Int }) id: number) {
     return this.userService.remove(id);
+  }
+
+  @Mutation(() => Boolean)
+  public async changeUserBusinessOwnerStatus(
+    @Args('isBusinessOwner') isBusinessOwner: boolean,
+    @CurrentUser() user: IUserSummary,
+  ): Promise<boolean> {
+    return this.userService.changeBusinessOwnerStatus(
+      user.userId,
+      isBusinessOwner,
+    );
   }
 }
